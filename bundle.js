09df7f8c9277f19feaa14d6ff68e594b621d7752
段昊辰 <duanhaochen@126.com>
2021-03-11 10:48:23 +0800

    feat: 入口调整为和webpack保持一致

---
+++ bundle.js
@@ -0,0 +1,4 @@
+const bundle = require('@umijs/deps/compiled/webpack/5/bundle5');
+
+exports.bundle = bundle;
+exports.isWebpack5 = true;
--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const bundle5 = require('@umijs/deps/compiled/webpack/5/bundle5');
+const { bundle } = require('./bundle');
 
-// 根据webpack5的出口导出webpack
-Object.defineProperty(exports, '__esModule', { value: true });
-
-exports.isWebpack5 = true;
-exports.bundle = bundle5;
-exports.default = exports.webpack = exports.__WEBPACK__ = bundle5().webpack;
+module.exports = bundle().webpack;
